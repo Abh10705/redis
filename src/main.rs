2fa85872a45709dfb319d2b69ad1157c4df3508e
Abh10705 <yipsilon0705@gmail.com>
2025-07-05 00:52:07 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,39 @@
 #![allow(unused_imports)]
 use std::io::Read;
 use std::io::Write;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+//use std::net::TcpListener;
 
-use std::net::TcpListener;
-
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     
-    for stream in listener.incoming() {
-         match stream {
+    loop {
+        
+        let stream = listener.accept().await; 
+        match stream {
            
-            Ok(mut stream) => {
-                println!("accepted new connection plz, handle it!");
-             
+            Ok((mut stream,_)) => {
+                println!("accepted new connection plz, handle it plzzz !");
+                
+                tokio::spawn(async move {
+               
                 let mut buf = [0; 512];
                 loop {
-                    let read_count = stream.read(&mut buf).unwrap();
+                    let read_count = stream.read(&mut buf).await.unwrap();
                     if read_count == 0 {
                         break;
                     }
-
-                    stream.write(b"+PONG\r\n").unwrap();
+                
+                    stream.write(b"+PONG\r\n").await.unwrap();
                 }       
+            });
             } 
             Err(e) => {
                 println!("error: {}", e);
